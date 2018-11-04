9849ee546f5b40dd2ddcf9fb6fbe45104fc4bd1a
Geono <syaokun2744@gmail.com>
2018-11-04 17:52:42 +0900

    Set react debugger

---
+++ app/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
+
+const AppReducer = combineReducers({
+    form
+});
+
+export default AppReducer;
---
+++ openDebugger.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+open "rndebugger://set-debugger-loc?host=localhost&port=19001"
+
